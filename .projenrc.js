4e18742a55eb250010aae8c0f174f5188d13027e
Matt Wise <matt@nextdoor.com>
2022-11-04 10:25:11 -0700

    chore: project created with projen

---
+++ .projenrc.js
@@ -0,0 +1,15 @@
+const { awscdk } = require('projen');
+const project = new awscdk.AwsCdkConstructLibrary({
+  author: 'Matt Wise',
+  authorAddress: 'wise@wiredgeek.net',
+  cdkVersion: '2.50.0',
+  defaultReleaseBranch: 'main',
+  name: 'kube10k',
+  repositoryUrl: 'https://github.com/Kube10K/kube10k.git',
+
+  // deps: [],                /* Runtime dependencies of this module. */
+  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
+  // devDeps: [],             /* Build dependencies for this module. */
+  // packageName: undefined,  /* The "name" in package.json. */
+});
+project.synth();
