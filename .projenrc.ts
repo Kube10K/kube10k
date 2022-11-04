884da867474ee915ef15413c859c5ac2f092571d
Matt Wise <matt@nextdoor.com>
2022-11-04 10:30:07 -0700

    prettier

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,22 +1,24 @@
-import { awscdk } from 'projen';
+import { awscdk } from "projen";
 
 const project = new awscdk.AwsCdkConstructLibrary({
-  author: 'Matt Wise',
-  authorAddress: 'wise@wiredgeek.net',
-  cdkVersion: '2.50.0',
-  defaultReleaseBranch: 'main',
-  name: 'kube10k',
-  repositoryUrl: 'https://github.com/Kube10K/kube10k.git',
+  author: "Matt Wise",
+  authorAddress: "wise@wiredgeek.net",
+  stability: "experimental",
+  cdkVersion: "2.50.0",
+  defaultReleaseBranch: "main",
+  name: "kube10k",
+  repositoryUrl: "https://github.com/Kube10K/kube10k.git",
   projenrcTs: true /* https://projen.io/typescript.html */,
   gitignore: [
     // CDK Temp Data
-    'cdk.out',
-    'cdk.staging',
+    "cdk.out",
+    "cdk.staging",
 
     // VIM
-    '*.swp',
-    '*.swo',
+    "*.swp",
+    "*.swo",
   ],
+  prettier: true,
 
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
