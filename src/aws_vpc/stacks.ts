f5380850cc37d426647aa040c2da90fcd6143475
Matt Wise <matt@nextdoor.com>
2022-11-04 11:46:36 -0700

    DefaultVpc Construct

---
+++ src/aws_vpc/stacks.ts
@@ -0,0 +1,54 @@
+/** @format */
+
+import * as cdk from "aws-cdk-lib";
+import { IVpc } from "aws-cdk-lib/aws-ec2";
+import { Construct } from "constructs";
+import { DefaultVpc, DefaultVpcProps } from "./defaultvpc";
+
+export interface VpcStackProps {
+  /**
+   * Provides access to customize the VPC being created.
+   * Ref: {@link DefaultVpcProps}
+   */
+  readonly vpcProps?: DefaultVpcProps;
+}
+
+export class VpcStack extends cdk.Stack {
+  // Resource properties
+  public readonly vpc: IVpc;
+
+  constructor(scope: Construct, id: string, props?: VpcStackProps) {
+    super(scope, id);
+
+    // Create the VPC resource itself
+    let vpcProps: DefaultVpcProps = props?.vpcProps || {};
+    let vpcConstruct: DefaultVpc = new DefaultVpc(this, id, vpcProps);
+
+    /**
+     * Record the final awsec2.Vpc object itself in our properties so that it can be passed around to other stacks and
+     * components
+     */
+    this.vpc = vpcConstruct.resource;
+  }
+}
+
+export class NestedVpcStack extends cdk.NestedStack {
+  /**
+   * The VPC in which this Cluster was created
+   */
+  public readonly vpc: IVpc;
+
+  constructor(scope: Construct, id: string, props?: VpcStackProps) {
+    super(scope, id);
+
+    // Create the VPC resource itself
+    let vpcProps: DefaultVpcProps = props?.vpcProps || {};
+    let vpcConstruct: DefaultVpc = new DefaultVpc(this, id, vpcProps);
+
+    /**
+     * Record the final awsec2.Vpc object itself in our properties so that it
+     * can be passed around to other stacks and components
+     */
+    this.vpc = vpcConstruct.resource;
+  }
+}
---
+++ test/aws_vpc/defaultvpc.test.ts
@@ -0,0 +1,62 @@
+/** @format */
+
+import { Stack } from "aws-cdk-lib";
+import { Match, Template } from "aws-cdk-lib/assertions";
+import { DefaultVpc } from "../../src/aws_vpc/defaultvpc";
+
+describe("DefaultVpc", () => {
+  test("Create", () => {
+    // GIVEN
+    const stack = new Stack();
+    new DefaultVpc(stack, "TestVpc", {
+      name: "foo",
+      maxAZs: 2,
+      cidr: "10.0.0.0/16",
+    });
+
+    // COMPILE
+    const template = Template.fromStack(stack);
+
+    // ASSERT: 1 VPC created
+    template.resourceCountIs("AWS::EC2::VPC", 1);
+
+    // ASSERT: 2 public, 2 private
+    template.resourceCountIs("AWS::EC2::Subnet", 4);
+
+    // ASSERT: 2 Private Subnets created with the expected CIDR
+    template.hasResource("AWS::EC2::Subnet", {
+      Properties: {
+        CidrBlock: "10.0.0.0/20",
+        Tags: Match.arrayWith([
+          { Key: "Name", Value: "Default/TestVpc/Vpc/PrivateSubnet1" },
+        ]),
+      },
+    });
+    template.hasResource("AWS::EC2::Subnet", {
+      Properties: {
+        CidrBlock: "10.0.16.0/20",
+        Tags: Match.arrayWith([
+          { Key: "Name", Value: "Default/TestVpc/Vpc/PrivateSubnet2" },
+        ]),
+      },
+    });
+
+    // ASSERT: 2 Public Subnets cretaed with the expected CIDR
+    template.hasResource("AWS::EC2::Subnet", {
+      Properties: {
+        CidrBlock: "10.0.32.0/24",
+        Tags: Match.arrayWith([
+          { Key: "Name", Value: "Default/TestVpc/Vpc/PublicSubnet1" },
+        ]),
+      },
+    });
+    template.hasResource("AWS::EC2::Subnet", {
+      Properties: {
+        CidrBlock: "10.0.33.0/24",
+        Tags: Match.arrayWith([
+          { Key: "Name", Value: "Default/TestVpc/Vpc/PublicSubnet2" },
+        ]),
+      },
+    });
+  });
+});
